377e4fe4c6ae790e3ae2490abf26d494106eb2b2
hao276843248 <276843248@qq.com>
2017-05-16 23:53:08 +0800

    test

--- MMXResMIS/src/mmxresmis/test/Test.java
+++ MMXResMIS/src/mmxresmis/test/Test.java
@@ -5,7 +5,7 @@ import mmxresmis.view.LoginView;
 public class Test {
 
 	public static void main(String[] args) {
-
+//123123
 		new LoginView(); 
 		// new UserRegisterView();
 		// new KaitaiView();
